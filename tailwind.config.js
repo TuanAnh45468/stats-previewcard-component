320678153dc50334cae330c46e863347acbb8fa4
tuana <tuananh45468@gmail.com>
2023-05-04 10:46:41 +0700

    complete desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     colors: {
       "dark-blue": "hsl(233, 47%, 7%)",
       "desaturated-blue": "hsl(244, 38%, 16%)",
-      "soft-violet": "hsl(277, 64%, 61%)",
+      "soft-violet": "hsl(275, 58%, 61%)",
       white: "hsl(0, 0%, 100%)",
       "transparent-white-para": "hsla(0, 0%, 100%, 0.75)",
       "transparent-white-heading": "hsla(0, 0%, 100%, 0.6)",
